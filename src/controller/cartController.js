7bd31538bf903d7135dcf59312e4005de68e1180
Soumya Dutta <soumyakantid5@gmail.com>
2022-08-08 08:55:22 +0530

    Update cartController.js

--- src/controller/cartController.js
+++ src/controller/cartController.js
@@ -132,7 +132,7 @@ const updateCart = async (req, res) => {
           return res.status(404).send({ status: false, message: "cartId does'nt exist" })
       }
 
-      const productDetails = await productModel.findOne({ _id: productId, isDeleted: false })
+      const productDetails = await productModel.findOne({ _id: productId })
 
       if (!productDetails) {
           return res.status(404).send({ status: false, message: "productId doesn't exist" })
